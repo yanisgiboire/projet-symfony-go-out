02b47607d33b297580df265f209f4901b85653c3
deathtroll22 <deathtroll@laposte.net>
2024-02-26 14:48:17 +0100

    wip is mobile

--- src/Controller/GoOutController.php
+++ src/Controller/GoOutController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Status;
+use MobileDetectBundle\DeviceDetector\MobileDetectorInterface;
 
 #[Route('/user/go_out')]
 class GoOutController extends AbstractController
@@ -28,9 +29,9 @@ class GoOutController extends AbstractController
     }
 
     #[Route('/organize/new', name: 'app_go_out_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, MobileDetectorInterface $mobileDetector): Response
     {
-
+        $mobileDetector->isMobile();
         $goOut = new GoOut();
         $form = $this->createForm(GoOutType::class, $goOut);
         $form->handleRequest($request);
