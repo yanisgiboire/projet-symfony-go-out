ca0207cbc132af92202fe91473083fbcf1c57dbf
deathtroll22 <deathtroll@laposte.net>
2024-02-26 09:49:31 +0100

    instal de mobile detect

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    MobileDetectBundle\MobileDetectBundle::class => ['all' => true],
 ];
