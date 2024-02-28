19c96a20bd54544def0357aa182f5cb16d350e83
Florian Marois <flomarois@gmail.com>
2024-02-28 10:32:43 +0100

    fix merge doublon

--- src/Form/GoOutType.php
+++ src/Form/GoOutType.php
@@ -16,8 +16,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use App\Validator\Constraints\LimitDateInscription;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
 class GoOutType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
