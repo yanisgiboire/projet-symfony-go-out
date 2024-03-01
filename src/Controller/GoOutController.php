62822f7994e1ce4686308a9dedaee4de4b071eb8
deathtroll22 <deathtroll@laposte.net>
2024-03-01 10:29:37 +0100

    text break sur le card title

--- src/Controller/GoOutController.php
+++ src/Controller/GoOutController.php
@@ -26,7 +26,8 @@ class GoOutController extends BaseController
 {
     #[Route('/', name: 'app_go_out_index', methods: ['GET'])]
     public function index(SessionInterface $session, GoOutRepository $goOutRepository, SiteRepository $siteRepository, participantGoOutRepository $participantGoOutRepository, CheckGoOutStatusService $checkGoOutStatusService): Response
-    {
+    {   
+        /** @var User $user */
         $userID = $this->getUser()->getId();
         $searchParams = $session->get('search_params', []);
         $go_outs = $goOutRepository->findForIndex($userID);
